75b8a6a4591e3e257fc09fda0cfcd4d0ed152c58
Karthik Swamy <getspk@gmail.com>
2020-08-09 16:25:20 -0500

    Second Commit

---
+++ FriendOrganizer.UI/Data/FriendDataService.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using FriendOrganizer.Model;
+
+namespace FriendOrganizer.UI.Data
+{
+    public class FriendDataService : IFriendDataService
+    {
+        public IEnumerable<Friend> GetAll()
+        {
+            yield return new Friend { FirstName = "Karthik", LastName = "Swamy", Email = "kswamy@cquest.us" };
+            yield return new Friend { FirstName = "Yasoda", LastName = "Swamy", Email = "yasoda@cquest.us" };
+            yield return new Friend { FirstName = "Ajay", LastName = "Swamy", Email = "ajay@cquest.us" };
+            yield return new Friend { FirstName = "Shashu", LastName = "Swamy", Email = "shashu@cquest.us" };
+        }
+    }
+}
---
+++ FriendOrganizer.UI/ViewModel/MainViewModel.cs
@@ -0,0 +1,16 @@
+﻿using FriendOrganizer.Model;
+using System;
+using System.Collections.Generic;
+using System.Collections.ObjectModel;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace FriendOrganizer.UI.ViewModel
+{
+    public class MainViewModel
+    {
+        public ObservableCollection<Friend>  Friends { get; set; }
+
+    }
+}
